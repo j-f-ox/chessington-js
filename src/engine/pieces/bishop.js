271506f5be7fed4198ff05a39370f2c80f6e9668
Jamie Fox <Jamie.Fox@softwire.com>
2018-07-02 15:26:24 +0100

    Add bishop moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -6,6 +6,22 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let movesArray = [];
+        //let moveArray = [1, -1];
+        for (let i=-1; i<2; i+=2) { //i = -1, 1
+            for (let j=-1; j<2; j+=2) {
+                
+                let newRow = currentSquare.row + i;
+                let newCol = currentSquare.col + j;
+
+                while(-1 < newRow && newRow < 8 && -1 < newCol && newCol < 8) {
+                    movesArray.push({row: newRow, col:newCol});
+                    newRow += i;
+                    newCol += j;
+                }
+            }
+        }
+        return movesArray;
     }
 }
