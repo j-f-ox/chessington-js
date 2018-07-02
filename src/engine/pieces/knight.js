95c4f79e0476534445d97a4e01a8ee6710cb71f8
Jamie Fox <Jamie.Fox@softwire.com>
2018-07-02 15:51:59 +0100

    updated knight available moves function

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -6,6 +6,21 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let movesArray = [];
+        const oneUp = 1;
+        const oneDown = -1;
+        const twoUp = 2;
+        const twoDown = -2;
+        let possibleMoves = [
+            {row: oneUp, col: twoDown}, {row: oneUp, col: twoUp}, {row: oneDown, col: twoDown}, {row: oneDown, col: twoUp},
+            {row: twoUp, col: oneUp}, {row: twoUp, col: oneDown}, {row: twoDown, col: oneUp}, {row: twoDown, col: oneDown}
+        ];
+        for (let i=0; i<possibleMoves.length; i++){
+            let newRow = currentSquare.row + possibleMoves[i].row;
+            let newCol = currentSquare.col + possibleMoves[i].col;
+            movesArray.push({row: newRow, col: newCol})
+        }
+        return movesArray;
     }
 }
