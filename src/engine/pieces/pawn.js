58040c86a97ba3b85aef24ec6828f389532a2d44
Jamie Fox <Jamie.Fox@softwire.com>
2018-07-02 14:16:51 +0100

    Simple pawn moves

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Player from '../player';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +7,13 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let movesArray = [];
+        if(this.player === Player.WHITE) {
+            movesArray.push({row: currentSquare.row + 1, col: currentSquare.col});
+        } else {
+            movesArray.push({row: currentSquare.row - 1, col: currentSquare.col});
+        }
+        return movesArray;
     }
 }
